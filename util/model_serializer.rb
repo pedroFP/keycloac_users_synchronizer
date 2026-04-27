f4f1842bfb895e7a097e45108a7911fe80222195
pedro <pedro.araya.labra@protonmail.com>
2026-04-27 11:40:59 +0000

    upgrade model_serializer

--- util/model_serializer.rb
+++ util/model_serializer.rb
@@ -1,6 +1,8 @@
+# Allow model instances to be serialized as hash and json
+
 module ModelSerializer
   include ActiveModel::Serialization
- 
+
   def attributes
     instance_variables.each_with_object({}) do |var, hash|
       hash[var.to_s.delete('@')] = instance_variable_get(var)
@@ -8,6 +10,8 @@ module ModelSerializer
   end
 
   def to_h
-    serializable_hash
+    serializable_hash.transform_values do |value|
+      value.respond_to?(:to_h) ? value.to_h : value
+    end
   end
 end
