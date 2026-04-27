9180c4d1d382f37e1c6c98be45a3464d953affff
pedro <pedro.araya.labra@protonmail.com>
2026-04-27 14:04:53 +0000

    update user_producer

--- app/producers/user_producer.rb
+++ app/producers/user_producer.rb
@@ -4,12 +4,12 @@ class UserProducer
   KAFKA_TOPIC = 'keycloak.users'.freeze
 
   def initialize(user)
-    @payload = payload.to_h
+    @payload = user.to_h
     @key = user.keycloak_id
   end
-  
+
   def produce
     # WIP: gem not installed nor cofigured
-    Karafka.producer.produce_sync(topic: KAFKA_TOPIC, payload:, key:)
+    Karafka.producer.produce_sync(topic: KAFKA_TOPIC, payload: @payload.to_json, key: @key)
   end
 end
